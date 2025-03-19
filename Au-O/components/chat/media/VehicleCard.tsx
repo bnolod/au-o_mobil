708ec5ed4219c9fcec6e27465b8de7a7ce33bb2b
bnolod <sadfoolc@gmail.com>
2025-03-19 22:11:24 +0100

    car styling

--- Au-O/components/chat/media/VehicleCard.tsx
+++ Au-O/components/chat/media/VehicleCard.tsx
@@ -5,11 +5,13 @@ import { CommonStaticElementProps } from "@/constants/types";
 import { getCarByCarId } from "@/lib/ApiCalls/CarApiCalls";
 import { Car } from "@/lib/entity/Car";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 
 export default function VehicleCard({ sender, vehicleId, colorScheme, language }: CommonStaticElementProps & { vehicleId: number, sender: boolean }) {
   const [vehicle, setVehicle] = useState<Car | null>(null);
+
   async function init() {
     const res = await getCarByCarId(vehicleId.toString());
     if (res) {
@@ -21,13 +23,13 @@ export default function VehicleCard({ sender, vehicleId, colorScheme, language }
   }, [])
   if (vehicle)
   return (
-    <View className={`${sender ? "items-end" : "items-start highlight-themed"}`} >
+    <TouchableOpacity className={`flex my-2 flex-col overflow-hidden w-2/3 ${sender ? 'self-end secondary mr-2' : 'self-start highlight-themed ml-16'} rounded-xl px-6 py-2`} onPress={() => router.push({ pathname: '/(garage)/[id]', params: { id: vehicle.id } })}>
       <View >
         {
             getCarImage(vehicle.type, colorScheme, 230, 70, 1.8)
         }
         <View>
-          <ThemedText>
+          <ThemedText className="text-xl font-bold">
             {vehicle.manufacturer}
           </ThemedText>
           <ThemedText>
@@ -35,20 +37,20 @@ export default function VehicleCard({ sender, vehicleId, colorScheme, language }
           </ThemedText>
         </View>
       </View>
-      <View >
+      <View className="flex w-full flex-row p-4 justify-between gap-6">
         <View >
-          <MaterialCommunityIcons name="horse" size={24} color={Colors[colorScheme].text} />
-          <ThemedText >{vehicle.horsepower}</ThemedText>
+          <MaterialCommunityIcons name="horse" size={32} color={Colors[colorScheme].text} />
+          <ThemedText className="text-lg">{vehicle.horsepower}</ThemedText>
         </View>
         <View >
-          <MaterialCommunityIcons name="engine" size={24} color={Colors[colorScheme].text} />
-          <ThemedText >{vehicle.displacement}</ThemedText>
+          <MaterialCommunityIcons name="engine" size={32} color={Colors[colorScheme].text} />
+          <ThemedText className="text-lg" >{vehicle.displacement}</ThemedText>
         </View>
         <View >
-          <MaterialCommunityIcons name="calendar" size={24} color={Colors[colorScheme].text} />
-          <ThemedText >{vehicle.productionYear}</ThemedText>
+          <MaterialCommunityIcons name="calendar" size={32} color={Colors[colorScheme].text} />
+          <ThemedText className="text-lg" >{vehicle.productionYear}</ThemedText>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
